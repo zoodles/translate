49426ae6c8d1b3d237b7d91b01c0018e5a538920
Milan Novota <milan.novota@gmail.com>
2010-08-26 15:22:13 +0200

    Some Rails 3 fixes; Added routes directly into the plugin

---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+ActionController::Routing::Routes.draw do
+  match 'translate' => 'translate#index', :as => :translate_list
+  match 'translate/translate' => 'translate#translate', :as => :translate
+  match 'translate/reload' => 'translate#reload', :as => :translate_reload
+end
