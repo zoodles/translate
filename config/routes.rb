1216b11b4ae747b479828410d241f8acaa4a5660
Milan Novota <milan.novota@gmail.com>
2010-08-26 15:28:18 +0200

    Add routes only in development mode

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 ActionController::Routing::Routes.draw do
-  match 'translate' => 'translate#index', :as => :translate_list
-  match 'translate/translate' => 'translate#translate', :as => :translate
-  match 'translate/reload' => 'translate#reload', :as => :translate_reload
+  if Rails.env == 'development'
+    match 'translate' => 'translate#index', :as => :translate_list
+    match 'translate/translate' => 'translate#translate', :as => :translate
+    match 'translate/reload' => 'translate#reload', :as => :translate_reload
+  end
 end
