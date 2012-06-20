164f9ef6c51ca238c92ae4bb32d098693558f363
JayTeeSF <jonathan@his-service.net>
2012-06-19 20:00:41 -0700

    bug-fix: corrected translate route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   get 'translate' => 'translate#index', :as => :translate_list
-  post 'translate' => 'translate#translate', :as => :translate
+  post 'translate/translate' => 'translate#translate', :as => :translate
   get 'translate/reload' => 'translate#reload', :as => :translate_reload
 end
